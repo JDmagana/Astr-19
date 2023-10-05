5c00cdbae527174847f8d25d617b6e6a68a57d0e
JDmagana <146879216+JDmagana@users.noreply.github.com>
2023-10-04 22:14:01 -0700

    Prompt #1- ASTR 19
    This is Prompt #1.

---
+++ prompt-#1.py
@@ -0,0 +1,10 @@
+a = "My name is Jeremy Magana."
+print(a)
+b = " My pronouns are he/him."
+print(b)
+c = " My favorite movie is Spirited away by studio Ghibli."
+print(c)
+d = " My favorite food is, Mongolian BBQ."
+print(d)
+e = a+b+c+d
+print(e)
