14b5e2c9f8ce6cbc3b72400a28f0573d4ec78def
JDmagana <146879216+JDmagana@users.noreply.github.com>
2023-10-06 14:11:17 -0700

    Prompt #2
    Prompt number 2. Integers and floating numbers

---
+++ Prompt-#2.py
@@ -0,0 +1,25 @@
+#This program will print 
+#The sum of two floating point numbers
+#The difference between two integers 
+#the product of a floating point number 
+#and an integer 
+
+a = 12.2   #define a floating point number a equal to 12.2
+b = 24.6   #define a floating point number b equal to 24.6
+print("a+b = ", a+b)
+
+a+b == 36.8
+print(type(a+b))
+
+x = 15  #define an integer number x equal to 8
+y = 12   #define an integer number y equal to 12 
+print("x-y = ",x-y)
+
+x-y == 3
+print(type(x-y))
+
+print("b*x = ",b*x)
+print(type(b*x))
+
+
+
